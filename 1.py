0a09708dd1d2de702308429140f7ff4c6801457d
lytka <lytkaanton8@gmail.com>
2024-03-03 14:35:50 +0200

    Crated Calculator

--- 1.py
+++ 1.py
@@ -1,1 +1,8 @@
-print("Hello World")
+a = int(input('Введіть перше число: '))
+b =  int(input('Введіть друге число: '))
+
+print(f'{a} + {b} = {a + b}')
+
+c = int(input('введіть третє число: '))
+
+print(f'{a} * {c} = {a * c}')
